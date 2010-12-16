d20fe68e9755002b8ee8aaf83287dc9442a6f196
Corey Hart <corey@codenothing.com>
2010-12-15 23:48:32 -0800

    -Exception code review

--- src/lib/Exception.php
+++ src/lib/Exception.php
@@ -5,15 +5,27 @@
  * Corey Hart @ http://www.codenothing.com
  */
 
-// Custom exception handler for CSSCompression
+
 Class CSSCompression_Exception extends Exception
 {
+	/**
+	 * Custom exception handler
+	 *
+	 * @param (string) message: Error message
+	 * @param (int) code: Error code
+	 * @instance (Exception Instance) previous: Previous exception
+	 */
 	public function __construct( $message = 'Unknown Exception', $code = 0, Exception $previous = NULL ) {
 		parent::__construct( $message, $code, $previous );
 	}
 
+	/**
+	 * String version of this custom exception
+	 *
+	 * @params none
+	 */
 	public function __toString(){
-		return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+		return "CSSCompression Exception: [" . $this->code . "] " . $this->message . "\n";
 	}
 };
 
