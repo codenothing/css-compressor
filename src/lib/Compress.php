77a84c6d9816fe247fee800bc24d485f9c8cec46
Corey Hart <corey@codenothing.com>
2010-12-15 23:09:08 -0800

    -Compress code review

--- src/lib/Compress.php
+++ src/lib/Compress.php
@@ -10,6 +10,21 @@ Class CSSCompression_Compress
 	/**
 	 * Trim Patterns
 	 *
+	 * @param (array) options: Reference to options
+	 * @param (array) stats: Reference to stats
+	 * @param (regex) rsemicolon: Checks for semicolon without an escape '\' character before it
+	 * @param (regex) rcolon: Checks for colon without an escape '\' character before it
+	 * @param (regex) rspace: Checks for space without an escape '\' character before it
+	 */
+	private $options = array();
+	private $stats = array();
+	private $rsemicolon = "/(?<!\\\);/";
+	private $rcolon = "/(?<!\\\):/";
+	private $rspace = "/(?<!\\\)\s/";
+
+	/**
+	 * Other inner classes that get used within compression
+	 *
 	 * @class Control: Compression Controller
 	 * @class Trim: Trim Instance
 	 * @class Setup: Setup Instance
@@ -18,11 +33,7 @@ Class CSSCompression_Compress
 	 * @class Cleanup: Cleanup Instance
 	 * @class Organize: Organize Instance
 	 * @class Selectors: Selectors Instance
-	 * @param (array) options: Reference to options
-	 * @param (array) stats: Reference to stats
-	 * @param (regex) rsemicolon: Checks for semicolon without an escape '\' character before it
-	 * @param (regex) rcolon: Checks for colon without an escape '\' character before it
-	 * @param (regex) rspace: Checks for space without an escape '\' character before it
+	 * @param (array) others: List of above classes for copying
 	 */
 	private $Control;
 	private $Trim;
@@ -32,11 +43,15 @@ Class CSSCompression_Compress
 	private $Cleanup;
 	private $Organize;
 	private $Selectors;
-	private $options = array();
-	private $stats = array();
-	private $rsemicolon = "/(?<!\\\);/";
-	private $rcolon = "/(?<!\\\):/";
-	private $rspace = "/(?<!\\\)\s/";
+	private $others = array(
+		'Trim',
+		'Setup',
+		'Format',
+		'Combine',
+		'Cleanup',
+		'Organize',
+		'Selectors',
+	);
 
 	/**
 	 * Stash a reference to the controller on each instantiation
@@ -45,15 +60,12 @@ Class CSSCompression_Compress
 	 */
 	public function __construct( CSSCompression_Control $control ) {
 		$this->Control = $control;
-		$this->Trim = $control->Trim;
-		$this->Setup = $control->Setup;
-		$this->Format = $control->Format;
-		$this->Combine = $control->Combine;
-		$this->Cleanup = $control->Cleanup;
-		$this->Organize = $control->Organize;
-		$this->Selectors = $control->Selectors;
 		$this->options = &$control->Option->options;
 		$this->stats = &$control->stats;
+
+		foreach ( $this->others as $class ) {
+			$this->$class = $control->$class;
+		}
 	}
 
 	/**
@@ -63,39 +75,8 @@ Class CSSCompression_Compress
 	 */ 
 	public function compress( $css ) {
 		$setup = $this->setup( $css );
-
-		// Do selector specific compressions
-		$this->Selectors->selectors( $setup['selectors'] );
-
-		// Look at each group of properties as a whole, and compress/combine similiar definitions
-		$this->Combine->combine( $setup['selectors'], $setup['details'] );
-
-		// If order isn't important, run comination functions before and after compressions to catch all instances
-		// Be sure to prune before hand for higher chance of matching
-		if ( $this->options['organize'] ) {
-			$this->Cleanup->cleanup( $setup['selectors'], $setup['details'] );
-			$this->Organize->organize( $setup['selectors'], $setup['details'] );
-			$this->Combine->combine( $setup['selectors'], $setup['details'] );
-		}
-
-		// Do final maintenace work, remove injected property/values
-		$this->Cleanup->cleanup( $setup['selectors'], $setup['details'] );
-
-		// Run final counters before full cleanup
-		$this->finalCount( $setup['selectors'], $setup['details'] );
-
-		// Format css to users preference
-		$css = $this->Format->readability( $this->options['readability'], $setup['selectors'], $setup['details'] );
-
-		// Intros
-		foreach ( $setup as $value ) {
-			if ( $value && is_string( $value ) ) {
-				$css = $value . $css;
-			}
-		}
-
-		// Remove escapables
-		$css = $this->Cleanup->removeInjections( $css );
+		$setup = $this->rulesets( $setup );
+		$css = $this->readability( $setup );
 
 		// Attach plea to top of page with unknown blocks
 		if ( $this->options['add-unknown'] && count( $setup['unknown'] ) ) {
@@ -137,6 +118,35 @@ Class CSSCompression_Compress
 	}
 
 	/**
+	 * Focus compressions on each rule set
+	 *
+	 * @param (array) setup: Array containing selectors and rule sets
+	 */
+	private function rulesets( $setup ) {
+		// Do selector specific compressions
+		$this->Selectors->selectors( $setup['selectors'] );
+
+		// Look at each group of properties as a whole, and compress/combine similiar definitions
+		$this->Combine->combine( $setup['selectors'], $setup['details'] );
+
+		// If order isn't important, run comination functions before and after compressions to catch all instances
+		// Be sure to prune before hand for higher chance of matching
+		if ( $this->options['organize'] ) {
+			$this->Cleanup->cleanup( $setup['selectors'], $setup['details'] );
+			$this->Organize->organize( $setup['selectors'], $setup['details'] );
+			$this->Combine->combine( $setup['selectors'], $setup['details'] );
+		}
+
+		// Do final maintenace work, remove injected property/values
+		$this->Cleanup->cleanup( $setup['selectors'], $setup['details'] );
+
+		// Run final counters before full cleanup
+		$this->finalCount( $setup['selectors'], $setup['details'] );
+
+		return $setup;
+	}
+
+	/**
 	 * Runs final counts on selectors and props
 	 *
 	 * @param (array) selectors: Selector rules
@@ -159,6 +169,28 @@ Class CSSCompression_Compress
 
 		// Final count for stats
 		$this->stats['after']['time'] = microtime( true );
+	}
+
+	/**
+	 * Formats the compressed rule sets into a stylesheet
+	 *
+	 * @param (array) setup: Array containing selectors and rule sets
+	 */
+	private function readability( $setup ) {
+		// Format css to users preference
+		$css = $this->Format->readability( $this->options['readability'], $setup['selectors'], $setup['details'] );
+
+		// Intros
+		foreach ( $setup as $value ) {
+			if ( $value && is_string( $value ) ) {
+				$css = $value . $css;
+			}
+		}
+
+		// Remove escapables
+		$css = $this->Cleanup->removeInjections( $css );
+
+		return $css;
 	}
 
 	/**
