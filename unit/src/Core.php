261790d3e3dfb941f5693f72b6c57c12f59b65aa
Corey Hart <corey@codenothing.com>
2010-12-19 11:25:32 -0800

    -Adding singleton testing

--- unit/src/Core.php
+++ unit/src/Core.php
@@ -96,6 +96,9 @@ Class CSScompression_Test
 		// Test express compression
 		$this->express();
 
+		// Test singleton access
+		$this->singleton();
+
 		// Full sheet tests (security checks)
 		$this->reset();
 		$this->testSheets();
@@ -135,6 +138,8 @@ Class CSScompression_Test
 		// Stash copies of each of the common modes
 		foreach ( $this->modes as $mode => $options ) {
 			$this->instances[ $mode ] = new CSSCompression( $mode );
+			$singleton = CSSCompression::getInstance( $mode );
+			$singleton->mode( $mode );
 		}
 	}
 
@@ -329,6 +334,31 @@ Class CSScompression_Test
 	}
 
 	/**
+	 * Make sure express is working correctly
+	 *
+	 * @params none
+	 */
+	private function express(){
+		$content = file_get_contents( $this->original . 'pit.css' );
+		foreach ( $this->instances as $mode => $instance ) {
+			$this->mark( "CSSCompression.express", $mode, CSSCompression::express( $content, $mode ) === $instance->compress( $content ) );
+		}
+	}
+
+	/**
+	 * Singleton testing
+	 *
+	 * @params none
+	 */
+	private function singleton(){
+		$content = file_get_contents( $this->original . 'pit.css' );
+		foreach ( $this->instances as $mode => $instance ) {
+			$singleton = CSSCompression::getInstance( $mode );
+			$this->mark( "CSSCompression.getInstance", $mode, $singleton->compress( $content ) === $instance->compress( $content ) );
+		}
+	}
+
+	/**
 	 * Run all test sheets through full compressor to see outcome
 	 *
 	 * @params none
@@ -422,18 +452,6 @@ Class CSScompression_Test
 					}
 				}
 			}
-		}
-	}
-
-	/**
-	 * Make sure express is working correctly
-	 *
-	 * @params none
-	 */
-	private function express(){
-		$content = file_get_contents( $this->original . 'pit.css' );
-		foreach ( $this->instances as $mode => $instance ) {
-			$this->mark( "CSSCompression.express", $mode, CSSCompression::express( $content, $mode ) === $instance->compress( $content ) );
 		}
 	}
 
