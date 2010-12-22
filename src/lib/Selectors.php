c646143d36b0cd16283175ee757c04d0332bfaca
Corey Hart <corey@codenothing.com>
2010-12-22 09:17:56 -0800

    -Prevent strictid if selector starts with star hack

--- src/lib/Selectors.php
+++ src/lib/Selectors.php
@@ -199,15 +199,19 @@ Class CSSCompression_Selectors
 	 * @param (string) selector: CSS Selector
 	 */
 	private function strictid( $selector ) {
-		$parts = preg_split( $this->rcomma, $selector );
-		foreach ( $parts as &$s ) {
-			if ( preg_match( $this->rid, $s ) ) {
-				$p = preg_split( $this->rid, $s );
-				$s = '#' . array_pop( $p );
+		if ( $selector[ 0 ] != '*' ) {
+			$parts = preg_split( $this->rcomma, $selector );
+			foreach ( $parts as &$s ) {
+				if ( preg_match( $this->rid, $s ) ) {
+					$p = preg_split( $this->rid, $s );
+					$s = '#' . array_pop( $p );
+				}
 			}
+
+			$selector = implode( ',', $parts );
 		}
 
-		return implode( ',', $parts );
+		return $selector;
 	}
 
 	/**
